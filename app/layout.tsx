e8e9b2e1f4ab9be8600476fff24ca530a6f1f6db
ANANT PATEL <anant3163@gmail.com>
2026-02-12 12:39:16 +0530

    Update project code

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
+
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import ParallaxWrapper from "./wrapper";
+import SmoothScroll from "./SmoothScroll";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +30,13 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <SmoothScroll>
+           <ParallaxWrapper>
         {children}
+      </ParallaxWrapper>
+        </SmoothScroll>
+       
+        
       </body>
     </html>
   );
---
+++ app/temp.tsx
@@ -0,0 +1,12 @@
+"use client";
+import { Parallax } from "react-scroll-parallax";
+
+export default function Temp() {
+    return (
+
+        <Parallax speed={-15} className="">
+            <img src="/222.jpg" className="h-full w-full"></img>
+        </Parallax>
+
+    )
+}
